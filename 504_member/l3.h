8661436cfb267ae839386165210a7118263da77c
snowyunee <snowyunee@snowyunee-ubuntu.(none)>
2012-06-21 21:25:05 +0900

    고치는 중.

--- 504_member/l3.h
+++ 504_member/l3.h
@@ -4,92 +4,137 @@
 #include <math.h>
 #include <list>
 #include <assert.h>
+#include <set>
+#include <queue>
+#include <algorithm>
 
 class RectangleArea
 {
- public:
-  int minimumQueries(std::vector<std::string> known)
-  {
-    std::list<std::pair<int,int> > unknown_points;
-    for(auto i=known.begin(); i!=known.end(); ++i)
-    {
-      for(auto j=i->begin(); j!=i->end(); ++j)
-      {
-        if(*j == 'Y')
-          unknown_points.push_back(std::make_pair(i-known.begin(), j-i->begin()));
-      }
-    }
+public:
+	//int minimumQueries(std::vector<std::string> known)
+	//{
+	//	std::set<std::pair<int,int> > unknown_points;
+	//	std::set<std::pair<int,int> > known_points;
+	//	for(auto i=known.begin(); i!=known.end(); ++i)
+	//	{
+	//		for(auto j=i->begin(); j!=i->end(); ++j)
+	//		{
+	//			if(*j == 'Y')
+	//				known_points.insert(std::make_pair(i-known.begin(), j-i->begin()));
+	//			else
+	//				unknown_points.insert(std::make_pair(i-known.begin(), j-i->begin()));
+	//		}
+	//	}
+	//
+	//	std::queue<std::pair<int,int> > additional_known_points;
+	//	check_all(known_points, unknown_points, additional_known_points);
 
-    int r = 0;
-    do
-    {
-      check(unknown_points, known);
-      std::pair<int, int> query_point = (unknown_points.front());
-      unknown_points.pop_front();
-      ++r;
-      known[query_point.first][query_point.second] = 'Y';
-    }
-    while(0 < unknown_points.size());
-    return r;
-  }
+	//	int r = 0;
+	//	while(false == unknown_points.empty());
+	//	{
+	//		while(false == additional_known_points.empty())
+	//		{
+	//			std::pair<int, int> add = additional_known_points.front();
+	//			additional_known_points.pop();
+	//			add_one(add, known_points, unknown_points, additional_known_points);
+	//		}
 
-  void check(std::list<std::pair<int,int> >& unknown_points, std::vector<std::string>& known)
-  {
-    bool is_added = true;
-    while(is_added == true)
-    {
-      for(auto i=0; i<known.size(); ++i)
-      {
-        for(auto j=0; j<known[0].size(); ++j)
-        {
-          if(known[i][j] == 'Y') 
-          {
-            is_added = set_known(i, j, known, unknown_points);
-          }
-        }
-      }
-    }
-  }
+	//		std::pair<int, int> add = *(unknown_points.begin());
+	//		unknown_points.erase(unknown_points.begin());
+	//		++r;
+	//		add_one(add, known_points, unknown_points, additional_known_points);
+	//	}
+	//	return r;
+	//}
 
-  bool set_known(int y, int x, std::vector<std::string>& known, std::list<std::pair<int,int> >& unknown_points)
-  {
-    std::list<int> y_list;
-    for(int j=0; j<known[0].size(); ++j)
-    {
-      if(j==x) continue;
-      if(known[y][j] == 'Y') y_list.push_back(j);
-    }
-    std::list<int> x_list;
-    for(int j=0; j<known[0].size(); ++j)
-    {
-      if(j==x) continue;
-      if(known[j][x] == 'Y') x_list.push_back(j);
-    }
-    std::cout << "x_list" << x_list.size() << ", y" << y_list.size() << std::endl;
-    for(int i=0; i<x_list.size(); ++i)
-    {
-      for(int j=0; j<y_list.size(); ++j)
-      {
-        if(known[j][i] != 'Y')
-        {
-          std::cout << "j" << j << ", i" << i << std::endl;
-          known[j][i] == 'Y';
-          bool is_erase = false;
-          for(auto k=unknown_points.begin(); k!=unknown_points.end(); ++k)
-          {
-            if(k->first == j && k->second == i)
-            {
-              unknown_points.erase(k);
-              is_erase = true;
-              break;
-            }
-          }
-          assert(is_erase);
-          return true;
-        }
-      }
-    }
-    return false;
-  }
+	//void check_all(std::set<std::pair<int,int> >& known_points,
+	//		std::set<std::pair<int,int> >& unknown_points,
+	//		std::queue<std::pair<int,int> >& additional_known_points)
+	//{
+	//	if(known_points.empty()) return;
+	//	for(auto i=known_points.begin(); i!=known_points.end(); ++i)
+	//	{
+	//		for(auto j=i; j!=known_points.end(); ++j)
+	//		{
+	//			if(i->first == j->first || i->second == j->second)
+	//			   	continue;
+
+	//			std::pair<int, int> point1 = std::make_pair(i->first, j->second);
+	//			std::pair<int, int> point2 = std::make_pair(j->first, i->second);
+	//			if(known_points.find(point1) != known_points.end() &&
+	//				   	known_points.find(point2) == known_points.end())
+	//			{
+	//				size_t erase_size = unknown_points.erase(point1);
+	//				if(erase_size > 0)
+	//					additional_known_points.push(point1);
+	//			}
+	//			else if(known_points.find(point2) != known_points.end() &&
+	//					known_points.find(point1) == known_points.end())
+	//			{
+	//				size_t erase_size = unknown_points.erase(point2);
+	//				if(erase_size > 0)
+	//					additional_known_points.push(point2);
+	//			}
+
+	//		}
+	//	} 
+	//}
+
+	int minimumQueries(std::vector<std::string> known)
+	{
+		std::set<std::pair<int,int> > unknown_points;
+		std::set<std::pair<int,int> > known_points;
+		for(auto i=known.begin(); i!=known.end(); ++i)
+		{
+			for(auto j=i->begin(); j!=i->end(); ++j)
+			{
+				if(*j == 'Y')
+					known_points.insert(std::make_pair(i-known.begin(), j-i->begin()));
+				else
+					unknown_points.insert(std::make_pair(i-known.begin(), j-i->begin()));
+			}
+		}
+	
+		bool is_optimal = false;
+		while(is_optimal == false)
+		{
+			std::queue<std::pair<int,int> > wast_points;
+			is_optimal = !check_wast(known_points);
+		}
+
+		print(known_points);
+		return (known.size() + known[0].size() - 1 - known_points.size());
+	}
+
+	void print(std::set<std::pair<int,int> >& known_points)
+	{
+	}
+
+	bool check_wast(std::set<std::pair<int,int> >& known_points)
+	{
+		if(known_points.empty()) return false;
+
+		for(auto i=known_points.begin(); i!=known_points.end(); ++i)
+		{
+			for(auto j=i; j!=known_points.end(); ++j)
+			{
+				if(i->first == j->first || i->second == j->second)
+				   	continue;
+
+				std::pair<int, int> point1 = std::make_pair(i->first, j->second);
+				std::pair<int, int> point2 = std::make_pair(j->first, i->second);
+				if(known_points.find(point1) != known_points.end() &&
+					   	known_points.find(point2) != known_points.end())
+				{
+					size_t erase_size = known_points.erase(point1);
+					assert(erase_size == 1);
+					return true;
+				}
+			}
+		} 
+
+		return false;
+	}
+
 
 };
